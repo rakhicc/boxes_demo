e0ebdfc2cbc9d5f527f9b2722dc4d92188cf0c0c
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-10-12 13:54:26 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
-
+const Box=(props) => {
+  return ( <div className="box">
+  <div>
+<h2>{props.name}</h2>
+<p>Title: {props.title}</p>
+<p>Age: {props.age}</p>
+</div>
+</div> );
+    
+  
+};
 function App() {
   return (
     <div className="App">
-      
-        <h1>Hello world!</h1>
+<header>
+<h1>Logo</h1>
+</header>
+<main>
+  <Box name="Maria" title="CEO" age="29"/>
+  <Box name="Karin" title="Developer" age="29"/>
+  <Box name="Kati" title="Designer" age="25"/>
+</main>
+<footer>
+<p><small>Copyrights</small></p>
+</footer>
       
     </div>
   );
