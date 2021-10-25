ddc0fe369bb389a3f57a20d3a2fae6597903cdb5
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-10-25 12:34:07 +0300

    adding class componet MainClassComponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
-import './App.css';
-import Main from './components/Main';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import "./App.css";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainClassComponent from "./components/MainClassComponent";
 function App() {
   return (
     <div className="App">
-<Header />
-<Main/>
-    <Footer />  
+      <Header />
+      <MainClassComponent />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/MainClassComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Box from "./Box";
+
+class MainClassComponent extends Component {
+  state = {
+    persons: [
+      { name: "Maria", title: "CEO", age: "29" },
+      { name: "Karin", title: "Developer", age: "29" },
+      { name: "Rakhi", title: "Designer", age: "25" },
+    ],
+  };
+  render() {
+    return (
+      <main>
+        <Box
+          name={this.state.persons[0].name}
+          title={this.state.persons[0].title}
+          age={this.state.persons[0].age}
+        />
+        <Box
+          name={this.state.persons[1].name}
+          title={this.state.persons[1].title}
+          age={this.state.persons[1].age}
+        />
+        <Box
+          name={this.state.persons[2].name}
+          title={this.state.persons[2].title}
+          age={this.state.persons[2].age}
+        />
+      </main>
+    );
+  }
+}
+export default MainClassComponent;
