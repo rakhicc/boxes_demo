1748ba3219edaf03f486311f6ed4c571c35d7e6d
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-10-26 13:09:30 +0300

    added lists and keys

--- src/components/MainClassComponent.js
+++ src/components/MainClassComponent.js
@@ -4,9 +4,9 @@ import Box from "./Box";
 class MainClassComponent extends Component {
   state = {
     persons: [
-      { name: "Maria", title: "CEO", age: "29" },
-      { name: "Karin", title: "Developer", age: "29" },
-      { name: "Rakhi", title: "Designer", age: "25" },
+      { id: 1, name: "Maria", title: "CEO", age: "29" },
+      { id: 2, name: "Karin", title: "Developer", age: "29" },
+      { id: 3, name: "Rakhi", title: "Designer", age: "25" },
     ],
   };
 
@@ -16,21 +16,15 @@ class MainClassComponent extends Component {
   render() {
     return (
       <main>
-        <Box
-          name={this.state.persons[0].name}
-          title={this.state.persons[0].title}
-          age={this.state.persons[0].age}
-        />
-        <Box
-          name={this.state.persons[1].name}
-          title={this.state.persons[1].title}
-          age={this.state.persons[1].age}
-        />
-        <Box
-          name={this.state.persons[2].name}
-          title={this.state.persons[2].title}
-          age={this.state.persons[2].age}
-        />
+        {this.state.persons.map((person) => (
+          <Box
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            title={person.title}
+          />
+        ))}
+
         <button onClick={this.changeNamehandler}>Click me</button>
       </main>
     );
